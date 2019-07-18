4278a150ed053a19dacd99827c40b7f239797db1
Sungwoo <sungwoo.cho.dev@gmail.com>
2019-07-18 20:38:04 +0900

    EnumType

--- packages/nexus-graphql/src/definitions/enumType.ts
+++ packages/nexus-graphql/src/definitions/enumType.ts
@@ -3,17 +3,17 @@ import { core } from 'nexus'
 import { isGraphqlSchemaBuilder, GraphqlSchemaBuilder } from '../builder'
 import { GraphqlEnumTypeNames, GraphqlEnumTypeValues } from '../types'
 
-interface PrismaEnumTypeConfig<TypeName extends string>
+interface GraphqlEnumTypeConfig<TypeName extends string>
   extends core.Omit<core.EnumTypeConfig<TypeName>, 'members'> {
   members: GraphqlEnumTypeValues<TypeName>[]
 }
 
 export function graphqlEnumType<TypeName extends GraphqlEnumTypeNames>(
-  typeConfig: PrismaEnumTypeConfig<TypeName>,
+  typeConfig: GraphqlEnumTypeConfig<TypeName>,
 ): core.NexusWrappedType<core.NexusEnumTypeDef<TypeName>> {
   return core.nexusWrappedType(typeConfig.name, builder => {
     if (!isGraphqlSchemaBuilder(builder)) {
-      throw new Error('prismaEnumType can only be used by `makePrismaSchema`')
+      throw new Error('graphqlEnumType can only be used by `makeGraphqlSchema`')
     }
 
     return nexusEnumType(typeConfig, builder)
@@ -21,12 +21,12 @@ export function graphqlEnumType<TypeName extends GraphqlEnumTypeNames>(
 }
 
 function nexusEnumType<TypeName extends string>(
-  typeConfig: PrismaEnumTypeConfig<TypeName>,
+  typeConfig: GraphqlEnumTypeConfig<TypeName>,
   builder: GraphqlSchemaBuilder,
 ): core.NexusEnumTypeDef<TypeName> {
   const typeName = typeConfig.name
-  const prismaSchema = builder.getDatamodelInfo().schema
-  const graphqlType = prismaSchema.getType(typeName)
+  const schema = builder.getDatamodelInfo().schema
+  const graphqlType = schema.getType(typeName)
   const members = getEnumTypeMembers(typeName, typeConfig.members, graphqlType)
   const description = typeConfig.description
     ? typeConfig.description
@@ -55,7 +55,7 @@ export function getEnumTypeMembers(
 
     if (!value) {
       throw new Error(
-        `Could not find ${graphqlType.name}.${member} in Prisma API`,
+        `Could not find ${graphqlType.name}.${member} in Graphql API`,
       )
     }
 
